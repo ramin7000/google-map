5d9404d70e7cf60ccdf43a85c481bc7cd2f7e6f0
Ramin Lohrasbi <mr.ramin7000@gmail.com>
2022-05-04 08:26:58 -0700

    Google Map start to show

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import GoogleMap from "./components/GoogleMap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <GoogleMap />
+        {/* 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        */}
       </header>
     </div>
   );
